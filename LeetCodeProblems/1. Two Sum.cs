abe723995a0918c1b178ecb4d28d621f8c3b72f4
Matheus <balaolmatheus@gmail.com>
2025-07-24 17:17:26 -0300

    Two Sum Exercise

---
+++ LeetCodeProblems/1. Two Sum.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        // Dictionary to store numbers and their indices for quick lookup
+        Dictionary<int, int> complementMap = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++) {
+            // Calculate the number needed to reach the target
+            int complement = target - nums[i];
+
+            // If the complement exists in the map, return the pair of indices
+            if (complementMap.ContainsKey(complement)) {
+                return new int[] { complementMap[complement], i };
+            }
+
+            // Otherwise, store the current number and its index
+            complementMap[nums[i]] = i;
+        }
+
+        // Return an empty array if no valid pair is found
+        return new int[0];
+    }
+}
