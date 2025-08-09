b16b67e2ebd516e8b4f7b7260a5dcbfe7df40b1f
LeonardiBalao <matheus.balao@hotmail.com>
2025-08-09 11:30:07 -0300

    Add 14. Longest Common Prefix Solution: Greedy, Devide & Conquer

---
+++ LeetCodeProblems/JavaScript/14. Longest Common Prefix.js
@@ -0,0 +1,17 @@
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) return "";
+    if (strs.length === 1) return strs[0];
+    
+    // Sort strings - common prefix will be between first and last
+    strs.sort();
+    const first = strs[0];
+    const last = strs[strs.length - 1];
+    
+    let i = 0;
+    // Find common prefix between first and last sorted strings
+    while (i < first.length && i < last.length && first[i] === last[i]) {
+        i++;
+    }
+    
+    return first.substring(0, i);
+};
