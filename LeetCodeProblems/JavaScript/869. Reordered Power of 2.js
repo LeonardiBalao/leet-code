4c4775680c7f3b2264d3456154469ba59bdb4397
LeonardiBalao <matheus.balao@hotmail.com>
2025-08-10 11:41:00 -0300

    Add Reordered Power of 2 Solution

---
+++ LeetCodeProblems/JavaScript/869. Reordered Power of 2.js
@@ -0,0 +1,20 @@
+var reorderedPowerOf2 = function(n) {
+    // Sort the digits of input number to create a pattern
+    const inputPattern = n.toString().split('').sort().join('');
+    
+    // Check all powers of 2 up to 10^9 (2^29 is the largest within constraint)
+    for (let power = 0; power < 30; power++) {
+        const powerOf2 = Math.pow(2, power);
+        
+        // Sort the digits of this power of 2 to create a pattern
+        const powerPattern = powerOf2.toString().split('').sort().join('');
+        
+        // If patterns match, we can rearrange input digits to form this power of 2
+        if (inputPattern === powerPattern) {
+            return true;
+        }
+    }
+    
+    // No matching power of 2 found
+    return false;
+};
