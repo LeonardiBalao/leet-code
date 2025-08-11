7b7f92d07b7a1a60778aeda1c6a52759345e608b
LeonardiBalao <matheus.balao@hotmail.com>
2025-08-11 19:41:34 -0300

    Maximum Subarray Solution: Kadane's Algorithm

---
+++ LeetCodeProblems/JavaScript/53. Maximum Subarray.js
@@ -0,0 +1,18 @@
+// Kadane's Algorithm
+
+var maxSubArray = function (nums) {
+    // Initialize maxSum and currentSum to the first element.
+    let maxSum = nums[0];
+    let currentSum = nums[0];
+
+    // Iterate through the array starting from the second element.
+    for (let i = 1; i < nums.length; i++) {
+        // Decide whether to add nums[i] to current subarray or start a new subarray at nums[i].
+        currentSum = Math.max(nums[i], currentSum + nums[i]);
+        // Update maxSum if currentSum is greater.
+        maxSum = Math.max(maxSum, currentSum);
+    }
+
+    // Return the largest sum found.
+    return maxSum;
+};
