0efb798d8b147cde6420475f6262c84484b981ac
LeonardiBalao <matheus.balao@hotmail.com>
2025-08-12 15:02:27 -0300

    Ways to Express an Integer as Sum of Powers Solution: Dynamic Programming - Improved version on top

--- LeetCodeProblems/JavaScript/2787. Ways to Express an Integer as Sum of Powers.js
+++ LeetCodeProblems/JavaScript/2787. Ways to Express an Integer as Sum of Powers.js
@@ -1,3 +1,37 @@
+// Improved version
+// Calculates the number of ways to sum up to 'n' using unique natural numbers raised to the power 'x'
+var numberOfWays = function (n, x) {
+    const MOD = 1e9 + 7; // Modulo constant to prevent integer overflow
+
+    // Find the largest integer k such that k^x <= n
+    let k = 1;
+    while (Math.pow(k, x) <= n) k++;
+    k--;
+
+    // DP table: dp[i][j] = number of ways to sum up to j using numbers 1..i (each raised to x)
+    let dp = Array.from({ length: k + 1 }, () => Array(n + 1).fill(0));
+    dp[0][0] = 1; // Base case: only one way to sum to 0 (use nothing)
+
+    // Fill DP table
+    for (let i = 1; i <= k; i++) {
+        let val = Math.pow(i, x); // Current number raised to power x
+        for (let j = 0; j <= n; j++) {
+            // Exclude current number: ways without using i
+            dp[i][j] = dp[i - 1][j];
+
+            // Include current number if possible: add ways to reach (j - val)
+            if (j >= val) {
+                dp[i][j] = (dp[i][j] + dp[i - 1][j - val]) % MOD;
+            } else {
+                dp[i][j] = dp[i][j] % MOD;
+            }
+        }
+    }
+
+    // Return the number of ways to sum to n using numbers up to k
+    return dp[k][n];
+};
+
 // Calculates the number of ways to sum up to 'n' using unique natural numbers raised to the power 'x'
 var numberOfWays = function (n, x) {
     const MOD = 1e9 + 7; // Modulo to prevent integer overflow
