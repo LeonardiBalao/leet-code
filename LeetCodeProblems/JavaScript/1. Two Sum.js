9951a553a363ba887eb71b5283a8b60cbfae0cfb
LeonardiBalao <matheus.balao@hotmail.com>
2025-08-21 21:50:23 -0300

    Two Sum Solution, Hashtables

---
+++ LeetCodeProblems/JavaScript/1. Two Sum.js
@@ -0,0 +1,21 @@
+var twoSum = function (nums, target) {
+    // Map for number-index pairs
+    const numMap = new Map();
+
+    // Loop through array
+    for (let i = 0; i < nums.length; i++) {
+        // Find needed complement
+        const complement = target - nums[i];
+
+        // If complement exists, return indices
+        if (numMap.has(complement)) {
+            return [i, numMap.get(complement)];
+        }
+        
+        // Store current number and index
+        numMap.set(nums[i], i);
+    }
+
+    // No solution (not reached per problem)
+    return [];
+};
