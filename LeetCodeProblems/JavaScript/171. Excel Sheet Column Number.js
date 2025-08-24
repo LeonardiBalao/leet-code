a4a2cc9ca128f361bc83e6844f474f84efcdfd50
LeonardiBalao <matheus.balao@hotmail.com>
2025-08-24 19:44:23 -0300

    Title to Number (Excel) solution, Base 26-system

---
+++ LeetCodeProblems/JavaScript/171. Excel Sheet Column Number.js
@@ -0,0 +1,14 @@
+function titleToNumber(columnTitle) {
+    const n = columnTitle.length;
+    let columnNumber = 0;
+
+    let i = n - 1;
+    while (i >= 0) {
+        // Convert character to 1-based value (A=1, B=2, ..., Z=26)
+        const charNumber = columnTitle.charCodeAt(i) - 'A'.charCodeAt(0) + 1;
+        // Update column number for base-26 system
+        columnNumber += charNumber * Math.pow(26, n - 1 - i);
+        i--;
+    }
+    return columnNumber;
+}
