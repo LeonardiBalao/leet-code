0823709c462961326f43c8eb4687ae6ab3dad72d
LeonardiBalao <matheus.balao@hotmail.com>
2025-08-26 21:31:14 -0300

    Reverse Bits Solution

---
+++ LeetCodeProblems/JavaScript/190. Reverse Bits.js
@@ -0,0 +1,11 @@
+function reverseBits(n) {
+    let result = 0;
+    for (let i = 0; i < 32; i++) {
+        // Extract the i-th bit from n
+        let bit = (n >> i) & 1;
+        // Place the bit in the (31-i)-th position of result
+        result |= (bit << (31 - i));
+    }
+    // Convert to unsigned 32-bit integer
+    return result >>> 0;
+}
