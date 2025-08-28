6a5f1a6b57ad90baa8b362505f70258462c14937
LeonardiBalao <matheus.balao@hotmail.com>
2025-08-27 21:04:29 -0300

    203. Remove Linked List Elements

---
+++ LeetCodeProblems/JavaScript/203. Remove Linked List Elements.js
@@ -0,0 +1,14 @@
+var removeElements = function (head, val) {
+    let dummy = new ListNode(0); // Create dummy node to simplify head removals
+    dummy.next = head;
+    let current = dummy; // Start at dummy node
+
+    while (current.next) { // Process until no next node
+        if (current.next.val === val) { // If next node has val, skip it
+            current.next = current.next.next;
+        } else { // Keep node, move to next
+            current = current.next;
+        }
+    }
+    return dummy.next; // Return modified list head
+};
