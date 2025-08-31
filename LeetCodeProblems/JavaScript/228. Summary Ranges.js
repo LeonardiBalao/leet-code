2081106d7d63d0bca93653fef19fec4919c72f5f
LeonardiBalao <matheus.balao@hotmail.com>
2025-08-30 22:23:25 -0300

    Summary Ranges Sliding Window

---
+++ LeetCodeProblems/JavaScript/228. Summary Ranges.js
@@ -0,0 +1,23 @@
+function summaryRanges(nums) {
+    const result = [];
+    if (nums.length === 0) {
+        return result;
+    }
+    
+    let left = 0;
+    for (let right = 0; right < nums.length; right++) {
+        // Check if right is the end of array or next number is not consecutive
+        if (right === nums.length - 1 || nums[right + 1] !== nums[right] + 1) {
+            // Form the range from left to right
+            if (left === right) {
+                result.push(nums[left] + "");
+            } else {
+                result.push(nums[left] + "->" + nums[right]);
+            }
+            // Move left to the next position after right
+            left = right + 1;
+        }
+    }
+    
+    return result;
+}
