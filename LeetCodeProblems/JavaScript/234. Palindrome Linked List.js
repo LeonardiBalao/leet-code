74cbee4b40f04f46bb65feca241890526b8f3abe
LeonardiBalao <matheus.balao@hotmail.com>
2025-08-31 11:05:17 -0300

    Palindrome Linked List, Fast Slow, Reverse Second Half and Compare

---
+++ LeetCodeProblems/JavaScript/234. Palindrome Linked List.js
@@ -0,0 +1,33 @@
+var isPalindrome = function(head) {
+    let fast = head;
+    let slow = head;
+
+    // Find middle of the list
+    while (fast !== null && fast.next !== null) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+
+    let curr = slow;
+    let next = null;
+    let newHead = null;
+
+    // Reverse second half
+    while (curr !== null) {
+        next = curr.next;
+        curr.next = newHead;
+        newHead = curr;
+        curr = next;
+    }
+
+    // Compare first and reversed second halves
+    while (newHead !== null && head !== slow) {
+        if (newHead.val !== head.val) {
+            return false;
+        }
+        newHead = newHead.next;
+        head = head.next;
+    }
+
+    return true;
+};
