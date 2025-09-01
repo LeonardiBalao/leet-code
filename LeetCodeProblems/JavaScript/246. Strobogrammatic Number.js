eadb910cac9ec3623741584c95ea3a09bf572b6f
LeonardiBalao <matheus.balao@hotmail.com>
2025-09-01 19:45:40 -0300

    Strobogrammatic Number Solution, sliding window

---
+++ LeetCodeProblems/JavaScript/246. Strobogrammatic Number.js
@@ -0,0 +1,30 @@
+var isStrobogrammatic = function(num) {
+    // Map of digits to their rotated counterparts
+    const rotatedDigits = {
+        '0': '0',
+        '1': '1',
+        '6': '9',
+        '8': '8',
+        '9': '6'
+    };
+
+    let left = 0;
+    let right = num.length - 1;
+
+    // Compare digits from both ends
+    while (left <= right) {
+        const leftChar = num[left];
+        const rightChar = num[right];
+
+        // Check if digit is valid and matches its rotated pair
+        if (!(leftChar in rotatedDigits) || rotatedDigits[leftChar] !== rightChar) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    // All digit pairs are valid
+    return true;
+};
