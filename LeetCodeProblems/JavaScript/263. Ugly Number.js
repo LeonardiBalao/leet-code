296255813fa437064f3917267ac39d6656e692f8
LeonardiBalao <matheus.balao@hotmail.com>
2025-09-02 20:47:06 -0300

    Ugly Number Solution, math

---
+++ LeetCodeProblems/JavaScript/263. Ugly Number.js
@@ -0,0 +1,10 @@
+var isUgly = function (n) {
+    if (n <= 0) return false; // Ugly numbers must be positive
+    
+    // Divide by 2, 3, 5 as long as possible
+    while (n % 2 === 0) n /= 2;
+    while (n % 3 === 0) n /= 3;
+    while (n % 5 === 0) n /= 5;
+    
+    return n === 1; // True if n is reduced to 1
+};
