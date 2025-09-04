511ad29786db9f5f0b39644ad56e3a89aa92cd5a
LeonardiBalao <matheus.balao@hotmail.com>
2025-09-04 18:32:20 -0300

    Missing Number solution, Gauss

---
+++ LeetCodeProblems/JavaScript/268. Missing Number.js
@@ -0,0 +1,6 @@
+function missingNumber(numbers) {
+    const length = numbers.length;
+    const expectedSum = length * (length + 1) / 2; // Sum of numbers from 0 to n
+    const actualSum = numbers.reduce((sum, num) => sum + num, 0); // Sum of array elements
+    return expectedSum - actualSum;
+}
