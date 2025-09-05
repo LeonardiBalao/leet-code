6cc3baf61bd4dab844ec8417af5684ffbb8984bb
LeonardiBalao <matheus.balao@hotmail.com>
2025-09-05 17:22:06 -0300

    Move Zeroes, two pointers solution

---
+++ LeetCodeProblems/JavaScript/283. Move Zeroes.js
@@ -0,0 +1,18 @@
+var moveZeroes = function(nums) {
+    let n = nums.length;
+    let left = 0;
+    
+    // Move non-zero elements to the front
+    for (let right = 0; right < n; right++) {
+        if (nums[right] !== 0) {
+            nums[left] = nums[right];
+            left++;
+        }
+    }
+    
+    // Fill the rest with zeros
+    while (left < n) {
+        nums[left] = 0;
+        left++;
+    }
+};
